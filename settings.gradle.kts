258dcab7b554dcf77e9cc7fd89a247694cf0e28e
cgens67 <cgenshin587@gmail.com>
2026-06-16 01:14:07 +0800

    Update JitPack repository URL in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { setUrl("https://jitpack.io") }
+        maven { setUrl("https://www.jitpack.io") }
     }
 }
 
